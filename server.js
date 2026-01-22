e804f3ce940f363ef066ac01610910b1b5898884
Karl Andri <karlandri2006@hotmail.com>
2026-01-22 10:46:28 +0000

    Kláraði að setja upp Page + Details

--- server.js
+++ server.js
@@ -14,26 +14,60 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 const getTeams = () => {
-    const teams = fs.readFileSync(path.join(__dirname, 'src/data/teams.json'));
-    return JSON.parse(teams);
+    const data = fs.readFileSync(path.join(__dirname, 'src/data/teams.json'));
+    return JSON.parse(data);
 }
+
+const getMovies = () => {
+    const data = fs.readFileSync(path.join(__dirname, 'src/data/movies.json'));
+    return JSON.parse(data);
+}
+
+
 
 
 // Slóðir
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Forsíða' });
+    const movies = getMovies();
+    res.render('index', { title: 'Bíovefurinn', movies });
 });
+
+app.get('/movie/:id', (req, res) => {
+  const movies = getMovies();
+  const movie = movies.find(m => m.id === req.params.id);
+
+  if (!movie) {
+    return res.status(404).render('404', { title: 'Síða fannst ekki' });
+  }
+
+  res.render('movie-details', { title: movie.title, movie });
+});
+
+
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'Um Okkur' });
 });
 
+
+
 app.get('/teams', (req, res) => {
-    res.render('teams', { title: 'Teams' });
+    const teams = getTeams();
+    res.render('teams', { title: 'Teams', teams });
 });
 
-app.get('/team-details', (req, res) => {
-    res.render('team-details', { title: 'Team details' });
+
+
+app.get('/team/:id', (req, res) => {
+    const teams = getTeams();
+    const team = teams.find(t => t.id === req.params.id);
+
+    if (!team) {
+        return res.status(404).render('404', { title: 'Síða fannst ekki (404)' });
+    }
+
+    res.render('team-details', { title: team.title, team });            
+
 });
 
 
